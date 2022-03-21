d697f64bc41a8d2ae3948233f9b3bdb4290b276d
Enzo Yono <nicholas.khami@gmail.com>
2022-03-21 15:47:21 -0500

    added buttons and changed to QUSWAP

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import PhononLogo from './phonon_logo.svg'
+import QuswapLogo from './quswap_img_replacement.png'
 
 const user = {
   name: 'Tom Cook',
@@ -27,58 +27,32 @@ export default function App() {
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
                       <img
-                        className="h-48 w-48"
-                        src= { PhononLogo }
+                        className="h-16 w-55"
+                        src= { QuswapLogo }
                         alt="quswapExchange"
                       />
                     </div>
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="ml-3 relative">
-                        <div>
-                          <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                            <span className="sr-only">Open user menu</span>
-                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {/* {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))} */}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-700 hover:bg-indigo-600"
+                    >
+                      0x61684651684846
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex items-center ml-2 px-3 py-2 border-indigo-600/100 bg-sky-700 text-sm leading-4 font-medium rounded-md text-white hover:bg-sky-600"
+                    >
+                      16 QU Bonded | $50000
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex items-center ml-2 px-3 py-2 border-indigo-600/100 bg-gray-700 text-sm leading-4 font-medium rounded-md text-white hover:bg-gray-500"
+                    >
+                      $US
+                    </button>
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
